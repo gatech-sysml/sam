51eb5675f27cbf7a916ad9b45803548d968f06f1
Glenn Matlin <glenn.matlin@gmail.com>
2021-10-13 19:35:25 -0400

    coarse crop32 script for debugging

---
+++ bin/models/coarse/crop32/32__16_2.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+gpu=0
+dp=16
+wd=2
+
+cr=32
+kr=$((cr / 4))
+pd=$((cr / 8))
+
+
+log_path="logs/model/coarse/all/crop${cr}_kernel${kr}_padding${pd}/depth${dp}_width${wd}/"
+mkdir -p $log_path
+python -u src/train.py --gpu $gpu \
+  --coarse_classes \
+  --crop_size $cr --depth $dp --width_factor $wd |
+  tee "${log_path}model_coarse_all_crop${cr}_kernel${kr}_depth${dp}_width${wd}.log"
