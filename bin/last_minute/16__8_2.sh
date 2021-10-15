4d367e7f3da80c0b9f135717a730d79eb879c84e
Glenn Matlin <glenn.matlin@gmail.com>
2021-10-15 13:09:17 -0400

    drop un-needed arg in scripts

--- bin/last_minute/16__8_2.sh
+++ bin/last_minute/16__8_2.sh
@@ -8,7 +8,6 @@ wd=2
 
 mkdir -p "logs/model/last_minute/"
 python -u src/train_multiclass.py --gpu $gpu \
-  --coarse_classes \
   --crop_size $cr --kernel_size $kr \
   --depth $dp --width_factor $wd |
   tee "logs/model/last_minute/multiclass_crop${cr}_depth${dp}_width${wd}.log"
